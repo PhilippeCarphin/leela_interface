22d01b9826ebeb9ee89fd8f10aeb426b684245f2
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-03-09 05:27:10 -0500

    Using subprocess.Popen allows me to use the IO streams in a nice way

--- leela.py
+++ leela.py
@@ -1,13 +1,24 @@
-from multiprocessing import Process, Pipe
+#!/bin/python3
+import subprocess
+import time
 
-def f(conn):
-    conn.send([42, None, 'hello'])
-    conn.close()
+p = subprocess.Popen(['./bin/leela_0110_linux_x64'], stdout=subprocess.PIPE,
+        stdin=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=69, universal_newlines=True)
 
-if __name__ == '__main__':
-    parent_conn, child_conn = Pipe()
-    p = Process(target=f, args=(child_conn,))
-    p.start()
-    print(parent_conn.recv())  # prints "[42, None, 'hello']"
-    p.join()
+p.stdin.write('play white g4\n')
+p.stdin.write('genmove white\n')
+p.stdin.write('genmove black\n')
+p.stdin.write('genmove white\n')
+p.stdin.write('genmove white\n')
+out1 = p.stdout.readline()
+print("out1 = " + out1)
+p.stdin.write('genmove black\n')
 
+
+out2 = p.stdout.readline()
+out3 = p.stderr.readline()
+
+
+
+print("out2 = " + out2)
+print("out3 = " + out3)
